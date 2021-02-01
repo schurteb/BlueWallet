e0a4034bb1cc91b5e1edcc8e8df4e1e2bfddd4e3
schurteb <benjamin.schurtenberger@gmail.com>
2021-02-01 19:57:05 +0100

    Add EthereumUnit model
    Signed-off-by: schurteb <benjamin.schurtenberger@gmail.com>

---
+++ models/ethereumUnits.js
@@ -0,0 +1,11 @@
+export const EthereumUnit = Object.freeze({
+  BTC: 'ETH',
+  SATS: 'gwei',
+  LOCAL_CURRENCY: 'local_currency',
+  MAX: 'MAX',
+});
+
+export const EthereumChain = Object.freeze({
+  ONCHAIN: 'ONCHAIN',
+  OFFCHAIN: 'OFFCHAIN',
+});
